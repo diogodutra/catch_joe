be610e7f41b29e39e5a46647892996499ebaf1ae
Diogo Dutra <diogodutra@gmail.com>
2020-09-23 20:36:45 -0400

    Added plots of gender and location

--- Toptal Final Project.py
+++ Toptal Final Project.py
@@ -1,24 +1,72 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[22]:
 
 
 import pandas as pd
 import numpy as np
+import seaborn as sns
+from collections import Counter
 
 
-# In[4]:
+# # Load dataset
+
+# In[18]:
 
 
 file = 'data/dataset.json'
 
 df = pd.read_json(file)
 
+# create target label as a separate column for supervised learning
+df['joe'] = df['user_id'] == 0
+
 df.head()
 
 
-# In[23]:
+# # Data Exploration
+
+# ## Gender
+
+# The first feature to be explored is `gender`. Let's explore the hypothesis that Joe never changed their gender.
+
+# In[19]:
+
+
+sns.catplot(x='joe', hue='gender', kind='count', data=df)
+
+
+# It seems that Joe is a male, since there is no log on his name as female. This insight is useful to discard around 40% of the dataset.
+
+# ## Location
+
+# In[27]:
+
+
+locations = list(set(df['location']))
+print(len(locations), 'locations in the dataset:', *locations)
+
+
+# Let's now explore `location`. Unless Joe works in a cruising ship, probably he has limited variation of location around the globe so let's explore this hypothesis.
+
+# In[20]:
+
+
+sns.catplot(x='joe', hue='location', kind='count', data=df)
+
+
+# In[24]:
+
+
+Counter(df[df['joe']]['location'])
+
+
+# Joe has access logs from Paris, Chicago and Toronto only. This is helpful to disregard the other 18 locations.
+
+# # Save
+
+# In[3]:
 
 
 # convert Notebook to Python for better version control
