6b2f3c418c94b9d0a6fdf6233af327c8ea765c4a
Diogo Dutra <diogodutra@gmail.com>
2020-09-24 20:54:46 -0400

    Added Decision Tree

--- Toptal Final Project.py
+++ Toptal Final Project.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[70]:
 
 
 import pandas as pd
 import numpy as np
 import seaborn as sns
 from collections import Counter
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import precision_score, recall_score
 
 
 # # Load dataset
@@ -110,14 +114,14 @@ sns.catplot(x='joe', hue='browser', kind='count', data=df)
 
 # The previously mentioned features are good enough to safely tell whenever is not Joe. However, how many logs by chance match exactly at the same time all these features? 
 
-# In[12]:
+# In[17]:
 
 
 df_like_joe = df.copy()
 
 # extract set of single entries from Joe's logs
-filter_data = {feat: set(df[df['joe']][feat])
-               for feat in ['gender', 'location', 'os', 'browser', 'locale']}
+features = ['gender', 'location', 'os', 'browser', 'locale']
+filter_data = {feat: set(df[df['joe']][feat]) for feat in features}
 
 for feature, valid_entries in filter_data.items():
     df_like_joe = df_like_joe[df_like_joe[feature].isin(valid_entries)]
@@ -138,7 +142,7 @@ print('Like-Joe dataset contains', df_like_joe.shape[0], 'logs',
 # 
 # But how many of the left logs are our Joe indeed?
 
-# In[13]:
+# In[12]:
 
 
 count = Counter(df_like_joe['joe'])
@@ -150,7 +154,7 @@ is_joe = list(is_joe / is_joe.sum())
 print('False and True logs ratio from Joe:', ', '.join('{0:.1%}'.format(i) for i in is_joe))
 
 
-# In[14]:
+# In[13]:
 
 
 user_id_like_joe = set(df_like_joe['user_id'])
@@ -159,9 +163,68 @@ print(len(user_id_like_joe), 'total of user_id with same logs than Joe:', *user_
 
 # Despite the fact that the filter previously mentioned has efficiently removed most of the dataset, there are yet some logs from a few people with enough occurencies to be a majority over Joe. This is yet something to be tackled, since we don't want these people being taken as Joe.
 
+# # Decision Tree
+
+# Maybe, the cross combination of restrictions across different features is enough to find Joe out of the other few people above. For instance, Joe might be the only one who uses Firefox (`browser`) on Windows 10 (`os`).
+# 
+# Let's create a simple Decision Tree, train it on the categorical features and check it's performance to detect Joe.
+
+# In[71]:
+
+
+df_ml = df[features + ['joe',]].copy()
+
+df_ml[features] = df_ml[features].astype('category')
+
+le = dict()
+for feat in features + ['joe']:
+    le[feat] = LabelEncoder()
+    df_ml[feat] = le[feat].fit_transform(df_ml[feat])
+    
+    
+df_ml.head()
+
+
+# In[114]:
+
+
+from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+
+# In[133]:
+
+
+test_ratio = .5
+
+X, y = df_ml[features].values, df_ml['joe'].values
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=test_ratio, random_state=42)
+
+
+model = DecisionTreeClassifier() #KNeighborsClassifier(2) #BaggingClassifier(KNeighborsClassifier()) #AdaBoostClassifier() #RandomForestClassifier()
+model.fit(X_train, y_train)
+model.score
+
+
+# In[138]:
+
+
+y_pred = model.predict(X_test)
+
+print('{0:.0%}'.format(recall_score(y_pred, y_test)), 'of the Joe\'s accesses are detected.')
+print('{0:.0%}'.format(precision_score(y_pred, y_test)), 'of the Joe\'s detections are true.')
+
+
+# The Decision Tree presented an average performance. However, it is far from flawless since there are too many false detections that lead some accesses being misclassified as Joe's.
+# 
+# Other similar types of classifiers (ie: AdaBoostClassifier, BaggingClassifier, RandomForestClassifier
+# and KNeighborsClassifier) resulted in the performance, which indicates that we need to further explore the rest of the non-categorical features.
+
 # # Save
 
-# In[ ]:
+# In[14]:
 
 
 # convert Notebook to Python for better version control
