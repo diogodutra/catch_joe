4d59123312ddc0f3db051e47f0349fad8f5df3c6
Diogo Dutra <diogodutra@gmail.com>
2020-09-25 17:07:15 -0400

    Added Hour in plot and list of features

--- Toptal Final Project.py
+++ Toptal Final Project.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[70]:
+# In[1]:
 
 
 import pandas as pd
 import numpy as np
 import seaborn as sns
 from collections import Counter
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import precision_score, recall_score
+from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, RandomForestClassifier, GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 
 # # Load dataset
 
-# In[2]:
+# In[ ]:
 
 
 file = 'data/dataset.json'
@@ -35,7 +38,7 @@ df.head()
 
 # The first feature to be explored is `gender`. Let's explore the hypothesis that Joe never changed their gender.
 
-# In[3]:
+# In[ ]:
 
 
 sns.catplot(x='joe', hue='gender', kind='count', data=df)
@@ -45,7 +48,7 @@ sns.catplot(x='joe', hue='gender', kind='count', data=df)
 
 # ## Location
 
-# In[4]:
+# In[ ]:
 
 
 locations = list(set(df['location']))
@@ -54,13 +57,13 @@ print(len(locations), 'locations in the dataset:', *locations)
 
 # Let's now explore `location`. Unless Joe works in a cruising ship, probably he has limited variation of location around the globe so let's explore this hypothesis.
 
-# In[5]:
+# In[ ]:
 
 
 sns.catplot(x='joe', hue='location', kind='count', data=df)
 
 
-# In[6]:
+# In[ ]:
 
 
 Counter(df[df['joe']]['location'])
@@ -72,13 +75,13 @@ Counter(df[df['joe']]['location'])
 
 # Let's now explore `locale`. It is rare to find an active polyglot so let's explore this hypothesis.
 
-# In[7]:
+# In[ ]:
 
 
 sns.catplot(x='joe', hue='locale', kind='count', data=df)
 
 
-# In[8]:
+# In[ ]:
 
 
 Counter(df[df['joe']]['locale'])
@@ -90,7 +93,7 @@ Counter(df[df['joe']]['locale'])
 
 # If Joe is not a geek than he is probably using only one or two different `os`.
 
-# In[9]:
+# In[ ]:
 
 
 sns.catplot(x='joe', hue='os', kind='count', data=df)
@@ -102,7 +105,7 @@ sns.catplot(x='joe', hue='os', kind='count', data=df)
 
 # For the same reason explained before for the OS, Joe is probably using only a couple of `browsers`.
 
-# In[10]:
+# In[ ]:
 
 
 sns.catplot(x='joe', hue='browser', kind='count', data=df)
@@ -110,17 +113,37 @@ sns.catplot(x='joe', hue='browser', kind='count', data=df)
 
 # Again, Joe uses only Firefox and Chrome, leaving out Internet Explorer and Safari.
 
+# # Time of the day
+
+# Let's now verify the hypothesis that Joe accesses internet only in some specific ties of the day.
+
+# In[ ]:
+
+
+df['hour'] = [int(time.split(':')[0]) for time in df['time']]
+
+sns.catplot(x='joe', hue='hour', kind='count', data=df)
+
+
+# In[ ]:
+
+
+print('Hours of the day with Joe\'s accesses:', *set(df[df['joe']]['hour']))
+
+
+# It seems that Joe never accesses internet at some specific hours of the day. Therefore, this is yet another relevant information to be used by our classifier.
+
 # # Predictive Model
 
 # The previously mentioned features are good enough to safely tell whenever is not Joe. However, how many logs by chance match exactly at the same time all these features? 
 
-# In[17]:
+# In[ ]:
 
 
 df_like_joe = df.copy()
 
 # extract set of single entries from Joe's logs
-features = ['gender', 'location', 'os', 'browser', 'locale']
+features = ['gender', 'location', 'os', 'browser', 'locale', 'hour']
 filter_data = {feat: set(df[df['joe']][feat]) for feat in features}
 
 for feature, valid_entries in filter_data.items():
@@ -142,7 +165,7 @@ print('Like-Joe dataset contains', df_like_joe.shape[0], 'logs',
 # 
 # But how many of the left logs are our Joe indeed?
 
-# In[12]:
+# In[ ]:
 
 
 count = Counter(df_like_joe['joe'])
@@ -154,7 +177,7 @@ is_joe = list(is_joe / is_joe.sum())
 print('False and True logs ratio from Joe:', ', '.join('{0:.1%}'.format(i) for i in is_joe))
 
 
-# In[13]:
+# In[ ]:
 
 
 user_id_like_joe = set(df_like_joe['user_id'])
@@ -167,54 +190,62 @@ print(len(user_id_like_joe), 'total of user_id with same logs than Joe:', *user_
 
 # Maybe, the cross combination of restrictions across different features is enough to find Joe out of the other few people above. For instance, Joe might be the only one who uses Firefox (`browser`) on Windows 10 (`os`).
 # 
-# Let's create a simple Decision Tree, train it on the categorical features and check it's performance to detect Joe.
+# Let's create a simple Decision Tree, train it on the single-entries categorical features and check it's performance to detect Joe.
 
-# In[71]:
+# In[ ]:
 
 
+def categorize(df, features):
+
+    df_ml[features] = df_ml[features].astype('category')
+
+    le = dict()
+    for feat in features:
+        le[feat] = LabelEncoder()
+        df[feat] = le[feat].fit_transform(df[feat])
+        
+    return df, le
+        
+        
 df_ml = df[features + ['joe',]].copy()
-
-df_ml[features] = df_ml[features].astype('category')
-
-le = dict()
-for feat in features + ['joe']:
-    le[feat] = LabelEncoder()
-    df_ml[feat] = le[feat].fit_transform(df_ml[feat])
-    
-    
+df_ml, le = categorize(df_ml, features + ['joe'])
 df_ml.head()
 
 
-# In[114]:
+# In[ ]:
 
 
-from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
+def split_data(df, label, **kwargs):
+    features = set(df_ml.columns) - {label}
+    X, y = df[features].values, df[label].values
+    return train_test_split(X, y, **kwargs)
+    
+    
+X_train, X_test, y_train, y_test = split_data(
+    df_ml, 'joe', test_size=.5, random_state=42)
 
 
-# In[133]:
+# In[ ]:
 
 
-test_ratio = .5
-
-X, y = df_ml[features].values, df_ml['joe'].values
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=test_ratio, random_state=42)
+def create_model(X_train, y_train, classifier=DecisionTreeClassifier):
+    return classifier().fit(X_train, y_train)
 
 
-model = DecisionTreeClassifier() #KNeighborsClassifier(2) #BaggingClassifier(KNeighborsClassifier()) #AdaBoostClassifier() #RandomForestClassifier()
-model.fit(X_train, y_train)
-model.score
+model = create_model(X_train, y_train)
 
 
-# In[138]:
+# In[ ]:
 
 
+def print_scores(y_pred, y_test):
+    print('{0:.2%}'.format(accuracy_score(y_pred, y_test)), 'is the accuracy of the classifier.')
+    print('{0:.0%}'.format(recall_score(y_pred, y_test)), 'of the Joe\'s accesses are detected.')
+    print('{0:.0%}'.format(precision_score(y_pred, y_test)), 'of the Joe\'s detections are true.')
+    
+    
 y_pred = model.predict(X_test)
-
-print('{0:.0%}'.format(recall_score(y_pred, y_test)), 'of the Joe\'s accesses are detected.')
-print('{0:.0%}'.format(precision_score(y_pred, y_test)), 'of the Joe\'s detections are true.')
+print_scores(y_pred, y_test)
 
 
 # The Decision Tree presented an average performance. However, it is far from flawless since there are too many false detections that lead some accesses being misclassified as Joe's.
@@ -222,9 +253,55 @@ print('{0:.0%}'.format(precision_score(y_pred, y_test)), 'of the Joe\'s detectio
 # Other similar types of classifiers (ie: AdaBoostClassifier, BaggingClassifier, RandomForestClassifier
 # and KNeighborsClassifier) resulted in the performance, which indicates that we need to further explore the rest of the non-categorical features.
 
+# # Websites
+
+# Now, let's do some trick to include the `sites` that contains multiple-entries.
+
+# In[ ]:
+
+
+var3 = pd.DataFrame(pd.DataFrame(df['sites'].values.tolist()).stack().reset_index(level=1))
+var3.columns = ['keys', 'values']
+
+sites_keys = list(df['sites'].values[0][0].keys())
+for new_feat in sites_keys:
+    var3[new_feat] = [v.get(new_feat) for v in var3['values']]
+    
+var3 = var3.join(df).drop(columns={'keys', 'values', 'sites'})
+    
+var3.head()
+
+
+# In[ ]:
+
+
+df_ml = var3[features + ['site', 'joe',]].copy()
+df_ml, le = categorize(df_ml, features + ['site', 'joe'])
+
+
+X_train, X_test, y_train, y_test = split_data(
+    df_ml, 'joe', test_size=.5, random_state=42)
+
+
+# In[ ]:
+
+
+# options: DecisionTreeClassifier AdaBoostClassifier BaggingClassifier RandomForestClassifier KNeighborsClassifier GradientBoostingClassifier
+model = create_model(X_train, y_train, RandomForestClassifier)
+
+y_pred = model.predict(X_test)
+print_scores(y_pred, y_test)
+
+
+# The result is slightly worst than the previous classifier was trained without `sites` features. It seems that this information is not useful as it is.
+
+# # Day
+
+# Let's use date and
+
 # # Save
 
-# In[14]:
+# In[ ]:
 
 
 # convert Notebook to Python for better version control
